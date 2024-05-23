887453d68cd30a82770835151ebb6386f32332fa
thor-x-me <abhishekverm2018@gmail.com>
2024-05-23 23:57:46 +0530

    Added speech to text script

---
+++ stt.py
@@ -0,0 +1,21 @@
+import speech_recognition as sr
+
+
+def listen(box):
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+        audio = recognizer.listen(source)
+
+    try:
+        print("Recognizing...")
+        text = recognizer.recognize_google(audio)
+        print("You said: ", text)
+        return text
+    except Exception as e:
+        print("Error: ", str(e))
+        return ""
+
+
+print(listen("box"))
